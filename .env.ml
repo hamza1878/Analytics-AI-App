b8502d6ba519d1476ab39463640ef1c0cf45c4cc
Hamza Ben Sassi <hbs310771@gmail.com>
2026-06-12 20:19:10 +0100

    modify .requirements

--- .env.ml
+++ .env.ml
@@ -1,5 +1,5 @@
 # ── PostgreSQL ───────────────────────────────────────────────────────────────
-DATABASE_URL=postgresql://postgres:1878@localhost:5432/Moviroo_DB_V2
+DATABASE_URL=postgresql://neondb_owner:npg_WMOkPag6V5pH@ep-steep-mode-aiinrzm7-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
 
 # ── MLflow ───────────────────────────────────────────────────────────────────
 MLFLOW_TRACKING_URI=http://localhost:5000
